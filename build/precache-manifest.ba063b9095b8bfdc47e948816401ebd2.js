8d7e9dc8131bbe07fac852e29d747578ecf04893
unknown <fhalbiero@gmail.com>
2020-03-29 21:33:27 -0300

    upload build project

---
+++ build/precache-manifest.ba063b9095b8bfdc47e948816401ebd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5851f9ffe60df0b899927a1e4eab1cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "353565fbc35e59fc921d",
+    "url": "/static/css/main.6ffb6b87.chunk.css"
+  },
+  {
+    "revision": "cd124a33bca0b81f27f0",
+    "url": "/static/js/2.6274c6c7.chunk.js"
+  },
+  {
+    "revision": "29bd4e1c10813c0c14fca0c4aca4571b",
+    "url": "/static/js/2.6274c6c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353565fbc35e59fc921d",
+    "url": "/static/js/main.10e34b89.chunk.js"
+  },
+  {
+    "revision": "79eefbb7815170a52a15",
+    "url": "/static/js/runtime-main.ea70b20a.js"
+  }
+]);
