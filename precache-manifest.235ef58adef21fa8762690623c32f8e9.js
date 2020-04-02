50abfa1e3a5379bf370d2a1888df2de66404001a
fhalbiero <fhalbiero@gmail.com>
2020-04-02 11:26:13 -0300

    Updates

--- precache-manifest.235ef58adef21fa8762690623c32f8e9.js
+++ precache-manifest.235ef58adef21fa8762690623c32f8e9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b5a228888f9a80b714b2d498e9952fa1",
+    "revision": "3b40eec5e80ead899de0faea85548f68",
     "url": "/coronavirus-tracker/index.html"
   },
   {
-    "revision": "9d45aa205045761e6fa6",
+    "revision": "81df99f08b9bce6b4919",
     "url": "/coronavirus-tracker/static/css/main.96887aec.chunk.css"
   },
   {
-    "revision": "e3c0f0da4b7355cdcbf1",
-    "url": "/coronavirus-tracker/static/js/2.d7965229.chunk.js"
+    "revision": "f10e7baa454f8e57d6e5",
+    "url": "/coronavirus-tracker/static/js/2.51f71c77.chunk.js"
   },
   {
     "revision": "68a0864388a93df58c5c0b0964c61030",
-    "url": "/coronavirus-tracker/static/js/2.d7965229.chunk.js.LICENSE.txt"
+    "url": "/coronavirus-tracker/static/js/2.51f71c77.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9d45aa205045761e6fa6",
-    "url": "/coronavirus-tracker/static/js/main.aab55529.chunk.js"
+    "revision": "81df99f08b9bce6b4919",
+    "url": "/coronavirus-tracker/static/js/main.ec7a485c.chunk.js"
   },
   {
     "revision": "44bec09273fd7d3b689e",
